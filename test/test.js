391e3444fc5e9f850deabe2c17ce3695f87101b0
IanZGray <ianzacharygray@gmail.com>
2023-02-14 14:08:32 -0500

    almost there

--- test/test.js
+++ test/test.js
@@ -1,13 +1,33 @@
-console.log = () => {}
-const { expect } = require('chai');
-const rewire = require('rewire');
-const Vue = require('vue');
+console.log = function() {};
+const { assert } = require('chai');
+const fs = require('fs');
+const Structured = require('structured');
+
+const code = fs.readFileSync('js/app.js', 'utf8');
 
 describe('', function () {
-    it('', function () {
-        let appModule = rewire("../js/app.js");
-        let vueApp = appModule.__get__("app");
-        expect(vueApp).to.exist();
-    });
+  it("Did you add a `submitButtonStyles` property on `computed` with a value of the provided function?", function () {
+    let structure = function() {
+      const app = new Vue({
+        computed: {
+          submitButtonStyles: function() {
+            if (this.formIsValid) {
+              return {
+                'background-color': '#4c7ef3',
+                cursor: 'pointer'
+              }
+            } else {
+              return {
+                'background-color': 'gray',
+                cursor: 'default'
+              }
+            }
+          }
+        }
+      });
+    };
+
+    let isMatch = Structured.match(code, structure);
+    assert.isOk(isMatch);
+  });
 });
-       
